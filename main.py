e7129b3036dab3ac6b8da9e74429181c74ad8b97
1k32k2 <79296275+1k32k2@users.noreply.github.com>
2023-11-21 19:13:10 +0000

    ready to build a docker and deploy

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
 import discord
 from discord.ext import tasks, commands
 import os
-
 import datetime
 import pytz
-
-target_date = datetime.datetime(2024, 2, 10, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
-current_date = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-remaining_days = (target_date - current_date).days #int
-# print(f"Remaining days until {target_date.date()}: {remaining_days}")
-
 
 client = commands.Bot(command_prefix='$', intents = discord.Intents.all())
 intents = discord.Intents.all()
 intents.message_content = True
-# client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
   print(f"log in as {client.user}: {client.user.id}")
   someloop.start()
 
-time=datetime.time(hour=1, minute=51, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
+time = datetime.time(hour = 0, minute = 0,tzinfo = pytz.timezone('Asia/Ho_Chi_Minh'))
 
-@tasks.loop(time=time)
+@tasks.loop(time = time)
 async def someloop():
   guild = client.get_guild(1053081261394116778)
   member = guild.get_member(920722041542426687)
   target_date = datetime.datetime(2024, 2, 10, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
   current_date = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
   remaining_days = (target_date - current_date).days
-  a='x'
-  await member.edit(nick=x)
-  
+  await member.edit(nick = f'{remaining_days} ngày nữa là Tết')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
