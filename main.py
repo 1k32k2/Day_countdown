dcd23d760539cc552c3f5c68f99ed488bbb91f05
1k32k2 <79296275+1k32k2@users.noreply.github.com>
2023-11-28 02:39:04 +0000

    add new file and feature on main.py

--- main.py
+++ main.py
@@ -11,18 +11,34 @@ intents.message_content = True
 @client.event
 async def on_ready():
   print(f"log in as {client.user}: {client.user.id}")
-  someloop.start()
+  channel = client.get_channel(1053585169182896228)
+  # await channel.send('ON AIR!')
+  # change_nickname.start()
+  change_channel_name.start()
 
-time = datetime.time(hour = 0, minute = 0,tzinfo = pytz.timezone('Asia/Ho_Chi_Minh'))
+time = datetime.time(hour = 0, minute = 0, tzinfo = pytz.timezone('Asia/Ho_Chi_Minh'))
 
-@tasks.loop(time = time)
-async def someloop():
+# @tasks.loop(time = time)
+# async def change_nickname():
+#   guild = client.get_guild(1053081261394116778)
+#   member = guild.get_member(920722041542426687)
+#   target_date = datetime.datetime(2024, 2, 10, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
+#   current_date = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+#   remaining_days = (target_date - current_date).days + 1
+#   if (remaining_days > 0):
+#     await member.edit(nick = f'{remaining_days} ngày nữa là Tết')
+#   else: 
+#     await member.edit(nick = 'Happy New Year!!!')
+
+@tasks.loop()
+async def change_channel_name():
   guild = client.get_guild(1053081261394116778)
-  member = guild.get_member(920722041542426687)
-  target_date = datetime.datetime(2024, 2, 10, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
+  channel = guild.get_channel(1178870060866085034)
+  target_date = datetime.datetime(2023, 12, 24, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
   current_date = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-  remaining_days = (target_date - current_date).days
-  await member.edit(nick = f'{remaining_days} ngày nữa là Tết')
+  remaining_days = target_date - current_date
+  print(remaining_days)
+  # await channel.edit(name = 'ewkjrhgjgh')
 
 intents = discord.Intents.default()
 intents.message_content = True
