4ee6614c3a8223c00046d426100429e5e942d80a
1k32k2 <79296275+1k32k2@users.noreply.github.com>
2023-11-30 06:33:48 +0000

    check the workflow, also complete the counting down to noel

--- main.py
+++ main.py
@@ -12,35 +12,46 @@ intents.message_content = True
 async def on_ready():
   print(f"log in as {client.user}: {client.user.id}")
   channel = client.get_channel(1053585169182896228)
-  # await channel.send('ON AIR!')
-  # change_nickname.start()
+  current_time = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+  await channel.send(f'ON AIR! {current_time}')
+  change_nickname.start()
   change_channel_name.start()
 
-time = datetime.time(hour = 0, minute = 0, tzinfo = pytz.timezone('Asia/Ho_Chi_Minh'))
+@tasks.loop(time = datetime.time(hour = 0, minute = 0, tzinfo = pytz.timezone('Asia/Ho_Chi_Minh')))
+async def change_nickname():
+  guild = client.get_guild(1053081261394116778)
+  member = guild.get_member(920722041542426687)
+  current_time = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+  target_date = datetime.datetime(2024, 2, 10, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
+  remaining_days = (target_date - current_time).days + 1
+  if (remaining_days > 0):
+    await member.edit(nick = f'{remaining_days} ngày nữa là Tết')
+  else: 
+    await member.edit(nick = 'Happy New Year!!!')
 
-# @tasks.loop(time = time)
-# async def change_nickname():
-#   guild = client.get_guild(1053081261394116778)
-#   member = guild.get_member(920722041542426687)
-#   target_date = datetime.datetime(2024, 2, 10, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
-#   current_date = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-#   remaining_days = (target_date - current_date).days + 1
-#   if (remaining_days > 0):
-#     await member.edit(nick = f'{remaining_days} ngày nữa là Tết')
-#   else: 
-#     await member.edit(nick = 'Happy New Year!!!')
-
-@tasks.loop()
+@tasks.loop(seconds = 1)
 async def change_channel_name():
   guild = client.get_guild(1053081261394116778)
   channel = guild.get_channel(1178870060866085034)
+  current_time = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
   target_date = datetime.datetime(2023, 12, 24, tzinfo=pytz.timezone('Asia/Ho_Chi_Minh'))
-  current_date = datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
-  remaining_days = target_date - current_date
-  print(remaining_days)
-  # await channel.edit(name = 'ewkjrhgjgh')
+  remaining_days = target_date - current_time
+  remaining_seconds = remaining_days.days * 24 * 3600 + remaining_days.seconds
+ 
+  days = remaining_seconds // (24 * 3600) 
+
+  remaining_seconds = remaining_seconds % (24 * 3600) 
+  hours = remaining_seconds // 3600
+
+  remaining_seconds %= 3600
+  minutes = remaining_seconds // 60
+
+  remaining_seconds %= 60
+  seconds = remaining_seconds
+  
+  await channel.edit(name = f'{days}D {hours}H {minutes}M {seconds}S to Noel')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client.run(os.environ['discord_token'])
+client.run(os.environ['DISCORD_TOKEN'])
